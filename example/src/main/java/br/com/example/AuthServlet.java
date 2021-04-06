765174f8cb67d3ec99f6c913e67977337638f1c1
Leonardo Torres <leonardotorres9@gmail.com>
2021-04-06 19:32:25 -0300

    add maven project

---
+++ example/src/main/java/br/com/example/AuthServlet.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.example;
+
+import javax.servlet.http.HttpServlet;
+
+/**
+ *
+ * @author leonardo
+ */
+public class AuthServlet extends HttpServlet {
+    
+}
